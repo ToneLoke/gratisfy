84ddaf168b0e2e5be3500fc271bac8f6a97d6c91
toneloke <guerrerotonyjs@gmail.com>
2017-11-02 16:46:02 -0400

    finished begin

---
+++ src/containers/Home.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import FlatButton from 'material-ui/FlatButton';
+import Toggle from 'material-ui/Toggle';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import {Link} from 'react-router-dom'
+
+class Login extends Component {
+  static muiName = 'FlatButton';
+
+  render() {
+    return (
+      <FlatButton {...this.props} label="Login" />
+    );
+  }
+}
+
+const Logged = (props) => (
+  <IconMenu
+    {...props}
+    iconButtonElement={
+      <IconButton><MoreVertIcon /></IconButton>
+    }
+    targetOrigin={{horizontal: 'right', vertical: 'top'}}
+    anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+  >
+    <Link to="/userprofile" ><MenuItem primaryText="Profile" /></Link>
+    <Link to="/exchange" ><MenuItem primaryText="Exchange Center" /></Link>
+    <MenuItem primaryText="Sign out" />
+  </IconMenu>
+);
+
+Logged.muiName = 'IconMenu';
+
+/**
+ * This example is taking advantage of the composability of the `AppBar`
+ * to render different components depending on the application state.
+ */
+class AppBarExampleComposition extends Component {
+  state = {
+    logged: true,
+  };
+
+  handleChange = (event, logged) => {
+    this.setState({logged: logged});
+  };
+
+  render() {
+    return (
+      <div>
+        <AppBar
+          title="Title"
+          iconElementLeft={<IconButton><NavigationClose /></IconButton>}
+          iconElementRight={this.state.logged ? <Logged /> : <Login />}
+        />
+      </div>
+    );
+  }
+}
+
+export default AppBarExampleComposition;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './containers/ExchangeContainer'
+import Exchange from './containers/ExchangeContainer'
 import UserPage from './containers/UserPage'
+import Home from './containers/Home'
 import registerServiceWorker from './registerServiceWorker'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
 
-// ReactDOM.render(<App />, document.getElementById('root'))
-ReactDOM.render(<MuiThemeProvider ><UserPage /></MuiThemeProvider>, document.getElementById('root'))
+const App = () => (
+  <Router>
+    <MuiThemeProvider >
+      <Route path='/' component={Home} />
+      <Route path='/userprofile' component={UserPage} />
+      <Route path='/exchange' component={Exchange} />
+    </MuiThemeProvider>
+  </Router>
+)
+ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
